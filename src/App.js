8436faf78afe3bc2842f60f15dd9d1f6d9a57b47
UniversMx <107148545+MxUnivers@users.noreply.github.com>
2022-07-13 16:52:14 +0200

    details & list-article

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React  from 'react';
 import './App.css';
+import {BrowserRouter as Router ,  Routes , Route} from  "react-router-dom"
+import Home from './pages/Home';
+import DetailsArticle from './pages/DetailsArticle';
+import ListArticles from './pages/ListArticles';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Router>     
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={Home} />
+          <Route path="/list-articles" element={<ListArticles/>} />
+          <Route path="/detail/id:" element={<DetailsArticle/>} />
+        </Routes>
+        <Footer/>
+      </Router>
     </div>
   );
-}
-
-export default App;
+}export default App;
